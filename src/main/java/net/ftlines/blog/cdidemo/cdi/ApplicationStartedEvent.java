b87df7526905f3fa2e30dbde34ea4aa25d689913
Victor Konopelko <vrebesbyrd@gmail.com>
2012-08-11 16:05:45 +0300

    delete some files

--- src/main/java/net/ftlines/blog/cdidemo/cdi/ApplicationStartedEvent.java
+++
@@ -1,8 +0,0 @@
-package net.ftlines.blog.cdidemo.cdi;
-
-/**
- * NB: this file is derived from the open-source project hosted at https://github.com/42Lines/blog-cdidemo
- */
-public class ApplicationStartedEvent {
-
-}
